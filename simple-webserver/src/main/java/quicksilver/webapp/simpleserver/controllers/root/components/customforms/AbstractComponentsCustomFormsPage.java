e9f4bdcd24ad1705b2b4680a2822457434e53f4e
Niels Gron <ndg@ndgs-MacBook-Pro-2.local>
2019-09-26 05:09:45 -0500

    Update License Headers
    Update License Headers

--- simple-webserver/src/main/java/quicksilver/webapp/simpleserver/controllers/root/components/customforms/AbstractComponentsCustomFormsPage.java
+++ simple-webserver/src/main/java/quicksilver/webapp/simpleserver/controllers/root/components/customforms/AbstractComponentsCustomFormsPage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Niels Gron and Contributors All Rights Reserved.
+ * Copyright 2018-2019 Niels Gron and Contributors All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package quicksilver.webapp.simpleserver.controllers.root.components.customforms;
 
 import quicksilver.webapp.simpleserver.controllers.root.components.AbstractComponentsPage;
