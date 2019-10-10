c38a434e25b41c79b1ae3c15e5e1be73b08cb0d4
Emilian Bold <emi@apache.org>
2019-10-10 10:50:45 +0300

    Copies test from DataFeedTest into unit test

--- simple-commons/src/test/java/quicksilver/commons/datafeed/DataFeedCSVTest.java
+++ simple-commons/src/test/java/quicksilver/commons/datafeed/DataFeedCSVTest.java
@@ -17,6 +17,17 @@ public class DataFeedCSVTest {
         assertEquals("2", t.getString(0, "b"));
         assertEquals("3", t.getString(0, "c"));
         assertEquals(3, t.columnCount());
+        System.out.println(t.structure());
+        System.out.println(t.print());
+    }
+
+    @Test
+    public void bushTest() throws IOException {
+        DataFeedCSV dataFeed = new DataFeedCSV("https://raw.githubusercontent.com/jtablesaw/tablesaw/master/data/bush.csv");
+        dataFeed.request();
+        Table dataTable = dataFeed.getDataTable();
+        System.out.println(dataTable.structure());
+        System.out.println(dataTable.print());
     }
 
 }
