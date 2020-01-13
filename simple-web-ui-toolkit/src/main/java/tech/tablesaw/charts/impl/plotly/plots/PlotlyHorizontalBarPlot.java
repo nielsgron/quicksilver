4c1067b1a5bc2f3c71de2edc2970ea53159579b0
Emilian Bold <emilian.bold@gmail.com>
2020-01-13 23:46:58 +0200

    Abstracts bar plot creation, thus adding columnForColor to horizontal bar plot too.

--- simple-web-ui-toolkit/src/main/java/tech/tablesaw/charts/impl/plotly/plots/PlotlyHorizontalBarPlot.java
+++ simple-web-ui-toolkit/src/main/java/tech/tablesaw/charts/impl/plotly/plots/PlotlyHorizontalBarPlot.java
@@ -1,38 +1,17 @@
 package tech.tablesaw.charts.impl.plotly.plots;
 
 import tech.tablesaw.charts.ChartBuilder;
-import tech.tablesaw.plotly.components.Figure;
 import tech.tablesaw.plotly.traces.BarTrace;
-import tech.tablesaw.plotly.traces.Trace;
 
-public class PlotlyHorizontalBarPlot extends PlotlyAbstractPlot {
+public class PlotlyHorizontalBarPlot extends PlotlyBarPlot {
 
     public PlotlyHorizontalBarPlot(ChartBuilder chartBuilder) {
-        setChartBuilder(chartBuilder);
-        String groupColName = columnsForViewColumns[0];
-        String[] numberColNames = columnsForViewRows;
+        super(chartBuilder);
+    }
 
-        // TODO : columnForLabels -
-        // TODO : columnForDetails -
-        // TODO : columnForColor -
-        // TODO : columnForSize -
-
-        // TODO : Support Clustered & Area display type. Research BarTrace.builder.mode(ScatterTrace.Mode.LINE) & BarTrace.builder.fill(ScatterTrace.Fill.TO_NEXT_Y) as in PlotlyAreaPlot
-
-        BarTrace.Orientation orientation = BarTrace.Orientation.HORIZONTAL;
-        Trace[] traces = new Trace[numberColNames.length];
-        for (int i = 0; i < numberColNames.length; i++) {
-            String name = numberColNames[i];
-            BarTrace trace =
-                    BarTrace.builder(table.categoricalColumn(groupColName), table.numberColumn(name))
-                            .orientation(orientation)
-                            .showLegend(true)
-                            .name(name)
-                            .build();
-            traces[i] = trace;
-        }
-
-        setFigure( new Figure(layout, traces) );
+    @Override
+    public BarTrace.Orientation getOrientation() {
+        return BarTrace.Orientation.HORIZONTAL;
     }
 
 }
