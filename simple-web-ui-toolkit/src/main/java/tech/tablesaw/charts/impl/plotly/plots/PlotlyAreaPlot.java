d43ca790022f1e03a6434f88dade8c1b199298b5
Emilian Bold <emilian.bold@gmail.com>
2020-02-01 16:25:10 +0200

    Color column on area plot.

--- simple-web-ui-toolkit/src/main/java/tech/tablesaw/charts/impl/plotly/plots/PlotlyAreaPlot.java
+++ simple-web-ui-toolkit/src/main/java/tech/tablesaw/charts/impl/plotly/plots/PlotlyAreaPlot.java
@@ -18,7 +18,6 @@ public class PlotlyAreaPlot extends PlotlyAbstractPlot {
 
         // TODO : columnForLabels -
         // TODO : columnForDetails -
-        // TODO : columnForColor -
         // TODO : columnForSize -
 
         List<Table> tableList;
@@ -31,19 +30,39 @@ public class PlotlyAreaPlot extends PlotlyAbstractPlot {
             tableList.add(table);
         }
 
-        ScatterTrace[] traces = new ScatterTrace[tableList.size()];
-        for (int i = 0; i < tableList.size(); i++) {
-            traces[i] =
-                    ScatterTrace.builder(
-                            tableList.get(i).numberColumn(xCol), tableList.get(i).numberColumn(yCol))
-                            .showLegend(true)
-                            .name(tableList.get(i).name())
-                            .mode(ScatterTrace.Mode.LINE)
-                            .fill(ScatterTrace.Fill.TO_NEXT_Y)
-                            .build();
-        }
+        if(columnForColor != null && !columnForColor.isEmpty()) {
+            Figure[] figures = tableList.stream()
+                    .map(tab -> {
+                        ScatterTrace[] traces = tab.splitOn(columnForColor).asTableList().stream()
+                                .map(t -> {
+                                    return ScatterTrace.builder(
+                                            t.numberColumn(xCol), t.numberColumn(yCol))
+                                            .showLegend(true)
+                                            .name(t.name())
+                                            .mode(ScatterTrace.Mode.LINE)
+                                            .fill(ScatterTrace.Fill.TO_NEXT_Y)
+                                            .build();
+                                })
+                                .toArray(ScatterTrace[]::new);
+                        return new Figure(layout, config, traces);
+                    })
+                    .toArray(Figure[]::new);
+            setFigures(figures);
+        } else {
+            ScatterTrace[] traces = new ScatterTrace[tableList.size()];
+            for (int i = 0; i < tableList.size(); i++) {
+                traces[i] =
+                        ScatterTrace.builder(
+                                tableList.get(i).numberColumn(xCol), tableList.get(i).numberColumn(yCol))
+                                .showLegend(true)
+                                .name(tableList.get(i).name())
+                                .mode(ScatterTrace.Mode.LINE)
+                                .fill(ScatterTrace.Fill.TO_NEXT_Y)
+                                .build();
+            }
 
-        setFigure( new Figure(layout, config, traces) );
+            setFigure( new Figure(layout, config, traces) );
+        }
     }
 
     public PlotlyAreaPlot(ChartBuilder chartBuilder) {
