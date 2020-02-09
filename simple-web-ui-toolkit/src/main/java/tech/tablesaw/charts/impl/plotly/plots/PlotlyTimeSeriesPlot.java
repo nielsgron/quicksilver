8f943c557156dc06e59ca3a75840848c1f0f91fd
Emilian Bold <emi@apache.org>
2020-02-09 14:18:47 +0200

    Column color and trace colors for time series

--- simple-web-ui-toolkit/src/main/java/tech/tablesaw/charts/impl/plotly/plots/PlotlyTimeSeriesPlot.java
+++ simple-web-ui-toolkit/src/main/java/tech/tablesaw/charts/impl/plotly/plots/PlotlyTimeSeriesPlot.java
@@ -8,17 +8,29 @@ import tech.tablesaw.table.TableSliceGroup;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import tech.tablesaw.plotly.components.Line;
 
 public class PlotlyTimeSeriesPlot extends PlotlyAbstractPlot {
+
+    private final static Logger LOG = LogManager.getLogger();
 
     public PlotlyTimeSeriesPlot(ChartBuilder chartBuilder, String groupCol) {
         setChartBuilder(chartBuilder);
         String dateColX = columnsForViewColumns[0];
+        if (columnsForViewColumns.length > 1) {
+            LOG.warn("Only one view column is supported but {} received", columnsForViewColumns.length);
+        }
+
         String yCol = columnsForViewRows[0];
+        if (columnsForViewRows.length > 1) {
+            //TODO: Support more columns in columnsForViewRows by creating more figures?
+            LOG.warn("Only one view row is supported but {} received", columnsForViewRows.length);
+        }
 
         // TODO : columnForLabels -
         // TODO : columnForDetails -
-        // TODO : columnForColor -
         // TODO : columnForSize -
 
         List<Table> tableList;
@@ -26,19 +38,40 @@ public class PlotlyTimeSeriesPlot extends PlotlyAbstractPlot {
         if ( groupCol != null ) {
             TableSliceGroup tables = table.splitOn(table.categoricalColumn(groupCol));
             tableList = tables.asTableList();
+
+            if (columnForColor != null) {
+                if (!columnForColor.equals(groupCol)) {
+                    LOG.warn("Cannot use a different color column when a group column is also present for time series plot: color column ignored.");
+                }
+            }
         } else {
-            tableList = new ArrayList<Table>();
-            tableList.add(table);
+            if (columnForColor == null) {
+                tableList = new ArrayList<Table>();
+                tableList.add(table);
+            } else {
+                TableSliceGroup colorTables = table.splitOn(table.categoricalColumn(columnForColor));
+                tableList = colorTables.asTableList();
+            }
         }
 
         ScatterTrace[] traces = new ScatterTrace[tableList.size()];
         for (int i = 0; i < tableList.size(); i++) {
             Table t = tableList.get(i).sortOn(dateColX);
-            traces[i] =
+            ScatterTrace.ScatterBuilder builder =
                     ScatterTrace.builder(t.dateColumn(dateColX), t.numberColumn(yCol))
                             .showLegend(true)
                             .name(tableList.get(i).name())
                             .mode(ScatterTrace.Mode.LINE)
+                            ;
+
+            if (columnForColor == null) {
+                if (traceColors != null && traceColors.length > i) {
+                    builder.line(Line.builder()
+                            .color(traceColors[i])
+                            .build());
+                }
+            }
+            traces[i] = builder
                             .build();
         }
 
