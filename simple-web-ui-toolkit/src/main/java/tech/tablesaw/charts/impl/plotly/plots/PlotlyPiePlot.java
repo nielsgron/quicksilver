1909d18d00c63b533bf979ce96b1264d6781ece5
Emilian Bold <emilian.bold@gmail.com>
2020-03-08 17:55:30 +0200

    Creates multiple pie plot figures for individual axes

--- simple-web-ui-toolkit/src/main/java/tech/tablesaw/charts/impl/plotly/plots/PlotlyPiePlot.java
+++ simple-web-ui-toolkit/src/main/java/tech/tablesaw/charts/impl/plotly/plots/PlotlyPiePlot.java
@@ -1,25 +1,49 @@
 package tech.tablesaw.charts.impl.plotly.plots;
 
+import java.util.stream.Stream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import tech.tablesaw.api.Table;
 import tech.tablesaw.charts.ChartBuilder;
 import tech.tablesaw.plotly.components.Figure;
 import tech.tablesaw.plotly.components.Marker;
 import tech.tablesaw.plotly.traces.PieTrace;
-import tech.tablesaw.plotly.traces.Trace;
 
 public class PlotlyPiePlot extends PlotlyAbstractPlot {
+
+    private final static Logger LOG = LogManager.getLogger();
 
     public PlotlyPiePlot(ChartBuilder chartBuilder) {
         setChartBuilder(chartBuilder);
         String groupColName = columnsForViewColumns[0];
-        String numberColName = columnsForViewRows[0];
+        if (columnsForViewColumns.length > 1) {
+            LOG.warn("Pie plot will only take into account the 1st view colum ({} received)", columnsForViewColumns.length);
+        }
 
         // TODO : columnForLabels -
         // TODO : columnForDetails -
         // TODO : columnForColor -
         // TODO : columnForSize -
+        if (individualAxes) {
+            setFigures(Stream.of(columnsForViewRows)
+                    .map(numberColName -> simpleFigure(table, groupColName, numberColName))
+                    .toArray(Figure[]::new));
+        } else {
+            //TODO: EMI: Add `domain` to trace
+            String numberColName = columnsForViewRows[0];
+            if (columnsForViewRows.length > 1) {
+                //TODO: EMI: there's no clean way to check if the layout has a grid... perhaps reflection or add public getters?
+                if (!layout.asJavascript().contains("grid:")) {
+                    LOG.warn("Pie plot has multiple view rows and shared axes but no layout.grid is defined");
+                }
+            }
+            setFigure(simpleFigure(table, groupColName, numberColName));
+        }
+    }
 
-        PieTrace.PieBuilder builder =
-                PieTrace.builder(table.column(groupColName), table.numberColumn(numberColName))
+    private Figure simpleFigure(Table table, String groupColName, String numberColName) {
+        PieTrace.PieBuilder builder
+                = PieTrace.builder(table.column(groupColName), table.numberColumn(numberColName))
                         .showLegend(true);
 
         if (columnForColor == null) {
@@ -32,7 +56,7 @@ public class PlotlyPiePlot extends PlotlyAbstractPlot {
         PieTrace trace = builder
                         .build();
 
-        setFigure( new Figure(layout, config, new Trace[]{trace}) );
+        return new Figure(layout, config, trace);
     }
 
 }
