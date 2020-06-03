4141c55d6165558d5f03f69f6900aa6585e73bb9
Emilian Bold <emilian.bold@gmail.com>
2020-06-03 18:52:44 +0300

    A more complex BSHyperlink that knows color classes

--- simple-web-ui-toolkit/src/main/java/quicksilver/webapp/simpleui/bootstrap4/components/BSHyperlink.java
+++ simple-web-ui-toolkit/src/main/java/quicksilver/webapp/simpleui/bootstrap4/components/BSHyperlink.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 Niels Gron and Contributors All Rights Reserved.
+ * Copyright 2018-2020 Niels Gron and Contributors All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package quicksilver.webapp.simpleui.bootstrap4.components;
 
-import quicksilver.webapp.simpleui.html.components.HTMLHyperlink;
+import quicksilver.webapp.simpleui.html.components.HTMLText;
 
 /*
     Example :
@@ -24,11 +23,43 @@ import quicksilver.webapp.simpleui.html.components.HTMLHyperlink;
     W3Schools : https://www.w3schools.com/tags/tag_a.asp
     Bootstrap Docs : NONE
  */
+public class BSHyperlink extends BSComponentContainer {
 
-public class BSHyperlink extends HTMLHyperlink {
+    private final String text;
+    private final String url;
 
     public BSHyperlink(String text, String hyperlinkURL) {
-        super(text, hyperlinkURL);
+        this.text = text;
+        this.url = hyperlinkURL;
+        add(new HTMLText(text));
+    }
+
+    @Override
+    protected void defineAttributes() {
+        putComponentAttribute(COMPONENT_ATTRIB_NAME, "Hyperlink");
+        putComponentAttribute(COMPONENT_ATTRIB_TAG_CLOSE, Boolean.TRUE);
+        putComponentAttribute(COMPONENT_ATTRIB_TAG_NAME, "a");
+        putComponentAttribute(COMPONENT_ATTRIB_END_WITH_LINEBREAK, Boolean.FALSE);
+
+        addTagAttribute("href", url);
+        addTagAttribute("class", getClassNames());
+    }
+
+    @Override
+    protected String getClassNames() {
+        if (getType() != null) {
+            return "text-" + getType().getTypeName();
+        }
+
+        return null;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getUrl() {
+        return url;
     }
 
 }
