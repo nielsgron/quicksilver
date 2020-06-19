1a7b9af622b5c4ee55b5a124518281e3899372c2
Emilian Bold <emilian.bold@gmail.com>
2020-06-19 20:03:43 +0300

    Partial scrollspy implementation

--- simple-web-ui-toolkit/src/main/java/quicksilver/webapp/simpleui/bootstrap4/components/BSScrollspy.java
+++ simple-web-ui-toolkit/src/main/java/quicksilver/webapp/simpleui/bootstrap4/components/BSScrollspy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 Niels Gron and Contributors All Rights Reserved.
+ * Copyright 2018-2020 Niels Gron and Contributors All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,26 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package quicksilver.webapp.simpleui.bootstrap4.components;
 
-import quicksilver.webapp.simpleui.HtmlStream;
+import quicksilver.webapp.simpleui.html.components.HTMLComponent;
 
-/*
-    Example :
-
-    W3Schools :
-    Bootstrap Docs :
+/**
+ * @see
+ * <a href='https://getbootstrap.com/docs/4.1/components/scrollspy/'>Bootstrap
+ * Docs</a>
  */
+public class BSScrollspy extends BSComponentContainer {
 
-public class BSScrollspy extends BSComponent {
+    private final String targetId;
+    private final String height;
 
-    protected void defineAttributes() {
+    public BSScrollspy(String targetId, String height) {
+        this.targetId = targetId;
+        this.height = height;
+    }
 
+    public BSScrollspy(HTMLComponent nav, String height) {
+        this(nav.getId(), height);
     }
 
     @Override
-    public void render(HtmlStream stream) {
+    protected void defineAttributes() {
+        putComponentAttribute(COMPONENT_ATTRIB_NAME, "Scrollspy");
+        putComponentAttribute(COMPONENT_ATTRIB_TAG_CLOSE, Boolean.TRUE);
+        putComponentAttribute(COMPONENT_ATTRIB_TAG_NAME, "div");
 
+        addTagAttribute("data-spy", "scroll");
+        addTagAttribute("data-target", "#" + targetId);
+        addTagAttribute("data-offset", "0");
+
+        addTagAttribute("style", "overflow-y: scroll; position: relative; height:" + height);
     }
 }
--- simple-webserver/src/main/java/quicksilver/webapp/simpleserver/controllers/root/components/bootstrap/Scrollspy.java
+++ simple-webserver/src/main/java/quicksilver/webapp/simpleserver/controllers/root/components/bootstrap/Scrollspy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 Niels Gron and Contributors All Rights Reserved.
+ * Copyright 2018-2020 Niels Gron and Contributors All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package quicksilver.webapp.simpleserver.controllers.root.components.bootstrap;
 
+import quicksilver.webapp.simpleui.bootstrap4.components.BSNavItem;
+import quicksilver.webapp.simpleui.bootstrap4.components.BSNavbar;
 import quicksilver.webapp.simpleui.bootstrap4.components.BSPanel;
 import quicksilver.webapp.simpleui.bootstrap4.components.BSScrollspy;
 import quicksilver.webapp.simpleui.bootstrap4.components.BSText;
+import quicksilver.webapp.simpleui.html.components.HTMLDiv;
 import quicksilver.webapp.simpleui.html.components.HTMLHeading;
 import quicksilver.webapp.simpleui.html.components.HTMLLineBreak;
 import quicksilver.webapp.simpleui.html.components.HTMLThematicBreak;
@@ -29,6 +31,7 @@ public class Scrollspy extends AbstractComponentsBootstrapPage {
         getSideBar().setActiveItem("Scrollspy");
     }
 
+    @Override
     protected BSPanel createContentPanelCenter() {
 
         BSPanel panel = new BSPanel();
@@ -37,9 +40,30 @@ public class Scrollspy extends AbstractComponentsBootstrapPage {
         panel.add(new HTMLHeading("Scrollspy Examples", 4));
         panel.add(new HTMLThematicBreak());
 
-        // Scrollspy Examples
+        BSNavbar nav = new BSNavbar(true, "MyBrand");
+        nav.id("navbar-example");
+        nav.add(new BSNavItem("Pricing", "#pricing"));
+        nav.add(new BSNavItem("Projects", "#projects"));
+        nav.add(new BSNavItem("About", "#about"));
 
-        panel.add(new BSScrollspy());
+        // Scrollspy Examples
+        BSScrollspy spy = new BSScrollspy(nav, "250px");
+
+        panel.add(nav);
+
+        spy.add(new HTMLHeading("Pricing", 1).id("pricing"));
+        spy.add(new BSText("Out pricing is the best." + generateText(12)));
+        spy.add(new HTMLLineBreak(2));
+
+        spy.add(new HTMLHeading("Projects", 1).id("projects"));
+        spy.add(new BSText("Previous projects are listed bellow..." + generateText(12)));
+        spy.add(new HTMLLineBreak(2));
+
+        spy.add(new HTMLHeading("About", 1).id("about"));
+        spy.add(new BSText("We are a small company built on a dream to..." + generateText(12)));
+        spy.add(new HTMLLineBreak(2));
+
+        panel.add(spy);
 
         return panel;
     }
